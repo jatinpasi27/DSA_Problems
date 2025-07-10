1261de4c9924ebc2c1349479b48af7c93c048fa7
jatin <pasijatin2004@gmail.com>
2025-07-10 17:44:29 +0530

    added array

---
+++ strings/str.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+int main(){
+    string str = "HELLO";
+    for (int i = 0; i < str.length(); i++)
+    {
+        cout<<str[i]<<" ";
+    }
+ return 0;   
+}
